7f0c20935fb755e8883de5d55694a41f68f45b17
Cayo <cayofemalara@gmail.com>
2024-08-29 23:02:45 -0300

    melhoria FAQ

--- js/collapse.js
+++ js/collapse.js
@@ -1,25 +1,36 @@
+var collapses = document.querySelectorAll(".collapse");
+var perguntas = document.querySelectorAll(".faq");
+var collapsesProntos = [];
+var atual = 0;
+
 $(document).ready(function () {
-    var collapses = document.querySelectorAll(".collapse");
-    var perguntas = document.querySelectorAll(".faq");
-
-    var collapsesProntos = [];
-
     for (let i = 0; i < collapses.length; i++) {
         var collapseNovo = new bootstrap.Collapse(collapses[i], {
             toggle: false
-        });   
+        });
         collapsesProntos.push(collapseNovo);
     }
 
     for (let i = 0; i < perguntas.length; i++) {
         perguntas[i].addEventListener('click', (ev) => {
-            for (let j = 0; j < collapsesProntos.length; j++) {
-                if(i == j)
-                    collapsesProntos[j].toggle();
-                else
-                    collapsesProntos[j].hide();
-            }
+            collapsesProntos[i].show();
+            atual = i;
+            fechaCollapses();
         });
     }
 });
 
+//Criei função pois o delay da animação não permite fazer a validação em um só looping
+function fechaCollapses() {
+    var collapsando = false;
+    for (let j = 0; j < collapsesProntos.length; j++) {
+        if (j != atual){
+            if (collapses[j].classList.contains("show"))
+                collapsesProntos[j].hide();
+            if (collapses[j].classList.contains("collapsing")) 
+                collapsando = true;
+        }
+    }
+    if(collapsando)
+        setTimeout(fechaCollapses, 500);
+}
